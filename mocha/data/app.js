d219fb1c18c3a3e0d2cc317cc4da8954cfd1a77d
Márton Gábor <gabor_marton1@epam.com>
2021-09-05 16:24:40 +0200

    mongo#3

--- mocha/data/app.js
+++ mocha/data/app.js
@@ -2,9 +2,7 @@ let mongoose = require("mongoose");
 const Job = require("./model");
 
 class DataProvider {
-  constructor() {
-    this.fData = [];
-  }
+  constructor() {}
 
   async connect() {
     await mongoose.connect("mongodb://localhost:27017/testDB");
@@ -12,6 +10,12 @@ class DataProvider {
 
   async disconnect() {
     await mongoose.disconnect();
+  }
+
+  async dropCollection() {
+    await this.connect();
+    await Job.collection.drop();
+    await this.disconnect();
   }
 
   async putData(data) {
@@ -30,69 +34,23 @@ class DataProvider {
     return jobs;
   }
 
+  async setupData(data) {
+    await this.putData(data);
+    return this.getData();
+  }
+
   async deleteData(data) {
     await this.connect();
     for (let delData of data) {
       await Job.deleteOne({ city: delData.city }, err => {
         if (err) console.log(err);
-      }).clone(); // clone nélkül nem hajlandó futtatni a parancsot, mert hogy már ez a query futtatva volt? nem is értettem
+      }).clone();
     }
-    //kókány megoldás de máshogy egyszerűen nem várta be a törléseket...
+
     setTimeout(async () => {
       await this.disconnect();
     }, 100);
   }
 }
+
 module.exports = DataProvider;
-
-/*let mongoose = require("mongoose");
-const Job = require("./model");
-const data = require("./data.json");
-let fData = [];
-
-async function connect() {
-  await mongoose.connect("mongodb://localhost:27017/testDB");
-}
-
-async function disconnect() {
-  await mongoose.disconnect();
-}
-
-async function putData() {
-  await connect();
-  for (let testData of data) {
-    const job = new Job(testData);
-    await job.save();
-  }
-  await disconnect();
-}
-
-async function getData() {
-  await connect();
-  const jobs = await Job.find();
-  await disconnect();
-  return jobs;
-}
-
-async function deleteData() {
-  await connect();
-  for (let delData of data) {
-    await Job.deleteOne({ city: delData.city }, err => {
-      if (err) console.log(err);
-      console.log("deleted");
-    }).clone();
-  }
-  setTimeout(async () => {
-    await disconnect();
-  }, 10);
-}
-
-async function main() {
-  await putData();
-  fData = await getData();
-  console.log(fData);
-  await deleteData();
-  return fData;
-}
-main().catch(err => console.log(err));
-*/
